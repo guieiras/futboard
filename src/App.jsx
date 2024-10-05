193b4b27cc63e14d5947af93e2bf5804ece319c2
guialbertoeiras@hotmail.com <guialbertoeiras@hotmail.com>
2024-10-05 11:51:09 -0300

    add menu to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Container, Menu } from 'semantic-ui-react'
 
 export default function App() {
   return (
-    <div>
-      <h1>My App</h1>
+    <div className="App">
+      <Menu inverted>
+        <Menu.Item header>Futboard</Menu.Item>
+      </Menu>
+
+      <Container>
+
+      </Container>
     </div>
   )
 }
